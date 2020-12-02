ec3ec0cfe9f3949a514c953a10205c8ede89af2d
Bridgit-wairimu <kirikabetty@gmail.com>
2020-12-03 01:47:09 +0300

    modified

--- config.py
+++ config.py
@@ -5,6 +5,18 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/550?api_key=fc3a024559a41bf4c9130f6e3345d852'
     MOVIE_API_KEY = os.environ.get(' fc3a024559a41bf4c9130f6e3345d852')
     SECRET_KEY = os.environ.get('1234')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:bridgit@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #email configurations
+    
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    
 
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 export MOVIE_API_KEY='fc3a024559a41bf4c9130f6e3345d852'
 export SECRET_KEY='1234'
 
+export MAIL_USERNAME=<Your Email Address>
+export MAIL_PASSWORD=<Your Email Password>
+
+
 python manage.py server
